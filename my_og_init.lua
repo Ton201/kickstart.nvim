a3cfccad1730fe048e5398cd288dae86a6214f80
Antonin Reh <antonin.reh@gmail.com>
2025-12-04 11:03:06 +0100

    Fix a typo

--- my_og_init.lua
+++ my_og_init.lua
@@ -1,6 +1,6 @@
 vim.cmd [[colorscheme evening]]
 vim.opt.number = true
--- sync nvim clipboard to the szstem clipboard
+-- sync nvim clipboard to the system clipboard
 vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = 'unnamedplus'
 end)
